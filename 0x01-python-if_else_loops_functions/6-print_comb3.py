0f9c52de5e4d01909e196c6d40fdde7a10573e51
MariamMahmoud28 <mm0376722@gmail.com>
2023-10-31 11:25:09 -0700

    task

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for i in range(10):
     for m in range(i, 10):
         if i < m:
-            print("{:d}{:d}".format(i, m),
-                    end="\n" if i == 8 and m == 9 else ", ")
+            print("{:d}{:d}".format(i, m), end=", " if i != 8 or m != 9 else "\n")
+
