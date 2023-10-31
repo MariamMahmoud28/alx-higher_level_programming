7da5399971b589f96a4847eb1eb229d7f07999e0
MariamMahmoud28 <mm0376722@gmail.com>
2023-10-31 11:16:34 -0700

    task

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for i in range(10):
+    for m in range(i, 10):
+        if i < m:
+            print("{:d}{:d}".format(i, m),
+                    end="\n" if i == 8 and m == 9 else ", "
