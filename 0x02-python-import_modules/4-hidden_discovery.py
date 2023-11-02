b28aaaa6274db9f75552b20107267124e5afac37
MariamMahmoud28 <mm0376722@gmail.com>
2023-11-02 11:18:33 -0700

    l

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
+if __name__ == "__main":
     import hidden_4
 
     for name in dir(hidden_4):
         if not name.startswith("__"):
-        print(name)
+            print(name)
+
