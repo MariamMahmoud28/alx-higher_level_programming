0a259fa8bf161317412bab64cc866365728019c5
MariamMahmoud28 <mm0376722@gmail.com>
2023-11-02 12:09:00 -0700

    l

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 import sys
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     args = sys.argv[1:]
 
-     total = 0
+    total = 0
     for arg in args:
         total += int(arg)
 
