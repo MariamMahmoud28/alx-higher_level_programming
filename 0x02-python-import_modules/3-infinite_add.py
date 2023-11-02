773d4dba2285b822b446f1a5c72ae72841d07ca1
MariamMahmoud28 <mm0376722@gmail.com>
2023-11-02 10:13:03 -0700

    t

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,13 +6,17 @@ if __name__ == "__main__":
     # Get all command-line arguments, excluding the script name (sys.argv[0])
     args = sys.argv[1:]
 
-    # Initialize a variable to store the sum
-    total = 0
+    # Check if there are any arguments
+    if args:
+        # Initialize a variable to store the sum
+        total = 0
 
-    # Loop through the arguments, cast each to an integer, and add to the total
-    for arg in args:
-        total += int(arg)
+        # Loop through the arguments, cast each to an integer, and add to the total
+        for arg in args:
+            total += int(arg)
 
-    # Print the total sum
-    print(total)
+        # Print the total sum
+        print(total)
+    else:
+        print("No arguments provided.")
 
