a476be71568e2b632ee0c84fb06ac9cc12f6f910
MariamMahmoud28 <mm0376722@gmail.com>
2023-11-06 12:47:54 -0800

    h

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    def max_integer(my_list=[]):
-    if not my_list:
-        return None
-
-    max_num = my_list[0]
-    for num in my_list[1:]:
-        if num > max_num:
-            max_num = num
-
-    return max_num
+    if not matrix:
+        return
+    for submatrix in matrix:
+        if len(submatrix) == 0:
+            print()
+        for i in range(len(submatrix)):
+            print("{:d}".format(submatrix[i]), end="\n" if i == len(submatrix) - 1 else " ")
