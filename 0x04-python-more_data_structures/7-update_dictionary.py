3925b28fd5cc3cd3f0538489c2cfdb80362bcc70
MariamMahmoud28 <mm0376722@gmail.com>
2023-11-08 12:48:46 -0800

    k

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
-
-# Example usage:
-my_dict = {'a': 1, 'b': 2, 'c': 3}
-update_dictionary(my_dict, 'b', 42)  # Replaces the value for 'b' key
-update_dictionary(my_dict, 'd', 99)  # Adds a new key 'd' with the value 99
-print(my_dict)
+    return a_dictionary
