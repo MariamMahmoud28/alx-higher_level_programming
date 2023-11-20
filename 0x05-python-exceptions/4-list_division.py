aba82b219529583c5ffd9ef4e5ef27fd6eb2c4ef
MariamMahmoud28 <mm0376722@gmail.com>
2023-11-20 12:08:12 -0800

    l

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -12,6 +12,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except (TypeError, ValueError):
             new_list.append(0)
             print("wrong type")
+            continue
         except IndexError:
             new_list.append(0)
             print("out of range")
