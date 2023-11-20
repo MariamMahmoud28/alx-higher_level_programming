d8cd546adcf55ddc1e06a11780401ad496487c0d
MariamMahmoud28 <mm0376722@gmail.com>
2023-11-20 10:25:27 -0800

    k

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
     try:
-        print("{:d}" .format(value))
+        print('{:d}' .format(value))
         return True
     except (TypeError, ValueError):
         return False
