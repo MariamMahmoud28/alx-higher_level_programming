0bbae2a9e36e6a65c3111bdbee6d040ca2bb4098
MariamMahmoud28 <mm0376722@gmail.com>
2023-11-21 11:10:55 -0800

    kk

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square moduule."""
+
+class Square:
+    """Defines a square."""
+
+    def __init__(seif, size):
+        """Constructor.
+
+        Args:
+            size: length of a side of the square.
+        """
+        self.__size = size
